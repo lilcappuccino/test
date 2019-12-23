900bf52682df85f2fdf565929f02a04d7d0c488f
lilcappucc <lilcappucc@gmail.com>
2019-12-23 17:59:34 +0200

    create base controllers/ change files hierarchy / add search view in ViewController

---
+++ TestINNOVATIONS/Search/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  TestINNOVATIONS
+//
+//  Created by dewill on 23.12.2019.
+//  Copyright © 2019 lilcappucc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var timer : Timer?
+    
+    // MARK:-> IBOutlet
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    // MARK:-> Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+        setupSearchbar()
+    }
+    
+    // MARK:-> Setup UI
+    private func setupNavigationBar(){
+        title = "Marchenko"
+    }
+    
+    private func setupSearchbar(){
+           let searchController = UISearchController(searchResultsController: nil)
+           searchController.hidesNavigationBarDuringPresentation = false
+           searchController.obscuresBackgroundDuringPresentation = false
+                navigationItem.searchController = searchController
+                searchController.searchBar.delegate = self
+       }
+    
+}
+
+//MARK:-> UISearchBarDelegate
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+           timer?.invalidate()
+           
+           timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: {_ in
+               print(searchText)
+
+           })
+       
+       }
+
+}
