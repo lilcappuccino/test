9e9d05db25dc21faec7f9639578f4a92c2f44f62
lilcappucc <lilcappucc@gmail.com>
2019-12-28 13:33:54 +0200

    add core data to project

---
+++ TestINNOVATIONS/Service/LocalDataService.swift
@@ -0,0 +1,18 @@
+//
+//  LocalDataService.swift
+//  TestINNOVATIONS
+//
+//  Created by dewill on 27.12.2019.
+//  Copyright © 2019 lilcappucc. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class LocalDataService {
+    
+    static func declarationDidAddToDb(in context: NSManagedObjectContext){
+    
+    }
+    
+}
--- TestINNOVATIONS/WebView/WebViewViewController.swift
+++ TestINNOVATIONS/WebView/WebViewViewController.swift
@@ -9,24 +9,25 @@
 import UIKit
 import WebKit
 
+//Using WebView for opening PDF File by url
 class WebViewViewController: UIViewController {
-
-   
+    
+    //   MARK:-> Outlet
     @IBOutlet weak var webView: WKWebView!
+    
     var link: String?
-           
-       
-       override func viewDidAppear(_ animated: Bool) {
-           super.viewDidAppear(true)
+    
+    //MARK:-> Lifecycle
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        loadData()
+    }
+    
+    private func loadData(){
+        guard let pdfLink = link , let url = URL(string: pdfLink) else { return }
+        webView.load(URLRequest(url: url))
         
-           loadData()
-       }
-       
-       private func loadData(){
-           guard let pdfLink = link , let url = URL(string: pdfLink) else { return }
-           webView.load(URLRequest(url: url))
-           
-       }
-       
-       
+    }
+    
+    
 }
